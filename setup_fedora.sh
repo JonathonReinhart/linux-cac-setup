f791a5986ac26ed7f3a2317c9b8fd9142daebe94
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2015-06-02 07:39:33 -0400

    fedora: add error() function and color output

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -2,11 +2,24 @@
 
 YUM_PKGS="pcsc-lite p11-kit coolkey openconnect"
 
+# setup colors
+if [ -t 1 ]; then
+    COLOR_RED="\033[31m"
+    COLOR_OFF="\033[0m"
+fi
+
+
+error() {
+    local message="$1"
+    local exitcode="${2:-2}" # default 2
+    echo -e "${COLOR_RED}Error: ${message}${COLOR_OFF}"
+    exit $exitcode
+}
+
 set -e
 
 if [ $(id -u) != "0" ]; then
-    echo "Must be run as root" >&2
-    exit 1
+    error "Must be run as root" 1
 fi
 
 echo -e "\nInstalling packages..."
